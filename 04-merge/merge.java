d05c438b32d52f59f62d46bdaae14f06f9d1bc78
jgelb <jgelb235@verizon.net>
2015-03-04 00:08:09 -0500

    infinite loop in final step.  Can't figure out why, because all of the helper functions are working.

--- 04-merge/merge.java
+++ 04-merge/merge.java
@@ -3,6 +3,9 @@ import java.util.*;
 public class merge {
     public static ArrayList<Integer> A = new ArrayList<Integer>();
     public static ArrayList<Integer> B = new ArrayList<Integer>();
+    public static int[] C = new int[20];
+    public static int[] D = new int[15];
+    
     Random r = new Random();
     public merge() {
 	A.add(10);
@@ -17,6 +20,17 @@ public class merge {
 	B.add(10);
 	B.add(12);
 	B.add(15);
+	int i = 0;
+	while (i < 20){
+	    C[i] = r.nextInt(50);
+	    i ++ ;
+	}
+	int j = 0;
+	while (j < 15){
+	    D[j] = r.nextInt(50);
+	    j ++ ;
+	}
+	
     }
     public static ArrayList<Integer> combine( ArrayList<Integer> A, ArrayList<Integer> B){
 	int i = 0;
@@ -61,12 +75,76 @@ public class merge {
 	}
         
     }
+
+
+    //-----------------------------------------------------------------------
+
+    public static int[] combine2( int[] A, int[] B){
+	int i = 0;
+	int j = 0;
+	int c = 0;
+        int[] ans = new int[A.length + B.length];
+	while (i < A.length && j < B.length ) {
+	    if (A[i] < B[j] ){
+		ans[c] = A[i] ;
+		i ++;
+		c ++;
+	    }
+	    else if (A[i] > B[j] ){
+		ans[c] = B[j];
+		j ++;
+		c ++;
+	    }
+	    else if (A[i] == B[j] ){
+		ans[c]=B[j];
+		c += 1;
+		i ++;
+		ans[c]=A[i];
+	        
+		j ++;
+	    }
+	   
+	}
+	while (i <A.length) { ans[c]= A[i] ;
+	    i++; c ++;}
+	while (j < B.length){ ans[c]=B[j] ;
+	    j++;c ++;}
+	return ans;
+}
+    public static int[] Sub2(int[] base, int first, int last ) {
+	int[] ans = new int[first + last + 10];
+	int c = 0;
+	for (int i = first; i<last; i ++) {
+	    ans[c]= base[i];
+	    c ++;
+	}
+	return ans;
+    }
+    public static int[] mergeSort2(int[] base ) {
+	int half = base.length / 2;
+	if (base.length <= 1 ) {
+	    return base;
+	}
+	if (base.length == 2){
+	    return combine2(Sub2(base , 0,1) , Sub2(base,1,2));
+	}
+	else {
+	    return  combine2 (mergeSort2 (Sub2(base,0,half)) , mergeSort2(Sub2(base, half , base.length)));
+	}
+        
+    }
+
+    //-----------------------------------------------------------------------------
     public static void main (String[] args){
 	merge x = new merge();
 	System.out.println(x.A);
-	System.out.println(x.B);
+	//System.out.println(x.B);
 	System.out.println();
 	//System.out.println(x.combine(A ,B));
 	System.out.println(mergeSort(x.A));
+	System.out.println(x.C);
+	System.out.println(x.combine2(x.C, x.D));
+	Sub2(x.C, 0, 12);
+	
     }
 }
