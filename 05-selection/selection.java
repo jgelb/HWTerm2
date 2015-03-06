1640633040a21b4494a36fe0355205a7dbd47334
jgelb <jgelb235@verizon.net>
2015-03-06 00:37:41 -0500

    done

--- 05-selection/selection.java
+++ 05-selection/selection.java
@@ -2,9 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class selection {
-    //choose a pivot value (random number)
-    //partition the list(rearrange so all values less are on left, and greater values on right
-    //now pivot is in its final location
-    // if k = location, done
-    // else, select on Upper or lower half//
+  
+    ArrayList<Integer> x = new ArrayList<Integer>();
+    ArrayList<Integer> y = new ArrayList<Integer>();
+    Random r = new Random();
+    int j = 0;
+    /*while (j < 20){
+	
+	x.add(r.nextInt(50));
+	j++;
+    }
+    */
+    int pivate = x.get(0);
+    public void doPivate(){
+	int k = 0;
+	y.add(pivate);
+	while (k < x.size()){
+	    if (x.get(k) > pivate){
+		y.add(x.get(k));
+	    }
+	    else if (x.get(k) < pivate){
+		y.add(0, x.get(k));
+	    }
+	    else if (x.get(k) == pivate){
+		y.add(y.indexOf(pivate), pivate);
+	    }
+	    k++ ;
+	}
+    }
+
+    public static void main (String[] args){
+	selection a = new selection();
+    }
+
 }
