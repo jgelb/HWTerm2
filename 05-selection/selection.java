bfb08a715cf2e8c521c4112758e1d9d56aacfc0a
jgelb <jgelb235@verizon.net>
2015-03-09 00:20:18 -0400

    something is slightly off with my final select recursive call.  Will try to fix tomorrow

--- 05-selection/selection.java
+++ 05-selection/selection.java
@@ -1,37 +1,8 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+public class Selection{
 
-public class selection {
-  
-    ArrayList<Integer> x = new ArrayList<Integer>();
-    ArrayList<Integer> y = new ArrayList<Integer>();
-    Random r = new Random();
-    int j = 0;
-    /*while (j < 20){
-	
-	x.add(r.nextInt(50));
-	j++;
-    }
-    */
-    int pivate = x.get(0);
-    public void doPivate(){
-	int k = 1;
-	y.add(pivate);
-	while (k < x.size()){
-	    if (x.get(k) > pivate){
-		y.add(x.get(k));
-	    }
-	    else if (x.get(k) < pivate){
-		y.add(0, x.get(k));
-	    }
-	    else if (x.get(k) == pivate){
-		y.add(y.indexOf(pivate), pivate);
-	    }
-	    k++;
-	}
-    }
-
-    /*
+ /*
 1 select pivot value
 2. swap pivot with a[h]
 3. li = l hi = h -1
@@ -39,11 +10,44 @@ public class selection {
 while li,h{
 if a[li] < p; li++
 else swap a[li] and a[hi]; hi --}
-
 then check for location
-     */
-    public static void main (String[] args){
-	selection a = new selection();
+*/
+    public int divide(int[] x , int SI, int EI) {
+        
+	int spot = x[SI];
+	System.out.println(Arrays.toString(x));
+	while(SI<EI) {
+	    int temp = x[SI];
+	    if (x[SI] < spot) {
+		SI += 1;
+	    }
+	    if(x[EI] > spot) {
+		EI -= 1;
+	    }
+
+	    x[SI] = x[EI];
+	    x[EI] = temp;
+	}
+        
+	System.out.println(Arrays.toString(x));
+	return SI;
+    }
+    public int select(int[] A ,int k, int l , int h) {
+        
+	if (k ==l) {
+	    return(divide(A,l,h));
+	}
+	else{
+	    return select(A,k,l+1,A.length-1);
+	}
     }
 
+    public static void main(String[] args) {
+	Selection a = new Selection() ;
+	int[] data = {2,3,16,20,0,9,4,1,65,73};
+        
+        System.out.println(Arrays.toString(data));
+	System.out.println((a.divide(data, 0 , data.length-1)));
+	//System.out.println(a.select(data,5,0,data.length-1));
+    }
 }
