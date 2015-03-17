414a65be15ce9d26017ef9bc0f1506e283f5aadc
jgelb <jgelb235@verizon.net>
2015-03-17 00:45:02 -0400

    find works, insert adds the node, but deletes the rest of the list after that node

---
+++ 07-lists1/LList.java
@@ -0,0 +1,39 @@
+public class LList {
+    private Node l=null;
+    public void add(String s){
+	Node tmp = new Node(s);
+	tmp.setNext(l);
+	l = tmp;
+    }
+    public Node find (int n){
+	Node tmp = l;
+	int count;
+	for (count = 1; count < n; tmp=tmp.getNext()){
+	    count += 1;
+	}
+	return tmp;
+    }
+    public void insert (int n, String s){
+	Node toAdd = new Node(s);
+	int count;
+	if (n == 0){
+	    toAdd.setNext(l);
+	    l = toAdd;
+	}
+	else{
+	    Node before = find(n - 1);
+	    before.setNext(toAdd);
+
+	}
+	    
+    }
+    public String toString(){
+	String s = "";
+	Node tmp;
+	for (tmp = l; tmp != null; tmp=tmp.getNext()){
+	    s = s + tmp + " --> ";
+	}
+	s = s + "null";
+	return s;
+    }
+}
