a225950957d6cab814e4adf188a811a7f06b490e
Joseph Gelb <jgelb235@verizon.net>
2015-03-23 08:26:19 -0400

    update

--- 09-iterators/LLit.java
+++ 09-iterators/LLit.java
@@ -1,5 +1,9 @@
 import java.io.*;
 import java.util.*;
+//pop returns top item in stack and removes it
+//push adds item to top of stack
+//empty returns true or false
+//peek returns but doesn't remove top
 
 public class LLit<E> implements Iterator<E>{
     private Node<E> t;
