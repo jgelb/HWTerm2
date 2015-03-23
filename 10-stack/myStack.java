4e6b145eab8813ea0c63329b3b3480178abc3850
Joseph Gelb <jgelb235@verizon.net>
2015-03-23 08:41:43 -0400

    update

--- 10-stack/myStack.java
+++ 10-stack/myStack.java
@@ -1,1 +1,12 @@
 //stack will contain nodes from a linked list
+
+public class myStack<E> {
+    private Node<E> dummy;
+    public myStack<E>(){
+	dummy = new Node<E>();
+    }
+    
+    public boolean empty(){
+	return dummy.getNext() == null;
+    }
+    public void push
