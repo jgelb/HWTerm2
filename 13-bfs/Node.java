c9f85500442f70bf1df4638b1bb54a33398e8ca6
jgelb <jgelb235@verizon.net>
2015-04-19 22:33:12 -0400

    Don't remember writing half of this code.  Must have done it in school last week.

--- 13-bfs/Node.java
+++ 13-bfs/Node.java
@@ -2,13 +2,22 @@
 public class Node {
     private int x,y;
     private Node prev;
-    private int priority;	
+    private int priority;
+    private int count; 
+    
     public Node(int x, int y){
 	this.x = x;
 	this.y = y;
+	count = 0;
 	priority = 0;
     }
+    public Node (int x, int y, int count){
+	this.x = x;
+	this.y = y;
+	this.count = count;
+    }
 
+    
     public Node getPrev() {
 	return prev;
     }
@@ -16,13 +25,26 @@ public class Node {
     public void setPrev(Node n){
 	prev = n;
     }
-
+    public int getCount (){
+	return count;
+    }
+    public void setCount(int n){
+	count = n;
+    }
     public int getPriority() {
 	return priority;
     }
 
     public void setPriority(int a, int b){
         priority = (a-x)*(a-x) + (b-y)*(b-y) ;
+    }
+    public void setManPriority(int a, int b){
+	priority = (a - x) + (b - y);
+    }
+    
+
+    public void setStar(int a, int b){
+	priority = (a-x)*(a-x) + (b-y)*(b-y) + count;
     }
 
     public int getX() {
@@ -33,6 +55,7 @@ public class Node {
 	return y;
     }
 
+    
     public String toString(){
 	return ""+priority;
     }
