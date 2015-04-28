8711ae6c32a0cc386d447c4195a683cc12de38f3
Joseph Gelb <jgelb235@verizon.net>
2015-04-28 08:35:27 -0400

    done

--- 14-bst/Tree.java
+++ 14-bst/Tree.java
@@ -1,3 +1,4 @@
+
 public class Tree{
     private Node root;
     public Tree(){
@@ -5,5 +6,83 @@ public class Tree{
     }
     public Tree(Node n){
 	root = n;
+    }
+    public void insert(int i){
+	Node t = root;
+	Node piggy = t;
+	if(t == null){
+	    t = new Node(i);
+	}
+	while(t!=null){
+	    piggy = t;
+	    if(t.getData()<i){
+		t=t.getRight();
+	    } else if(t.getData()>i) {
+		t=t.getLeft();
+	    } else {
+		return;
+	    }
+	}
+	if(piggy.getData() < i){
+	    piggy.setRight(new Node(i));
+	}else {
+	    piggy.setLeft(new Node(i));
+	}
+    }
+    public Node search(int i){
+	Node t = root;
+	while(t!=null){
+	    if(t.getData() < i){
+		t = t.getRight();
+	    }else if(t.getData()>i){
+		t=t.getLeft();
+	    }else {
+		return t;
+	    }
+	}
+	return null;
+    }
+    public Node rSearch(Node t, int i){
+	if(t==null){
+	    return null;
+	}
+	if(t.getData() < i){
+	    return rSearch(t.getRight(),i);
+	} else if (t.getData() > i){
+	    return rSearch(t.getLeft(),i);
+	}
+	return t;
+    }
+    public Node rSearch(int i){
+	return rSearch(root,i);
+    }
+    //not really sure how to format a tree but this atleast prints everything
+    public String traverse(Node t){
+	if(t == null){
+	    return "";
+	}
+	return t.getData() + ", " + traverse(t.getLeft()) + traverse(t.getRight());
+    }
+    public String toString(){
+	return traverse(root);
+    }
+    public String ascend(Node t){
+	if(t == null){
+	    return "";
+	}
+	return ascend(t.getLeft()) + t.getData() + ", " + ascend(t.getRight());
+    }
+    public String ascend(){
+	return ascend(root);
+    }
+    public static void main(String[] args){
+	Random rnd = new Random();
+	Node r = new Node(40);
+	Tree b = new Tree(r);
+	for(int i=0;i<10;i++){
+	    b.insert(10*rnd.nextInt(10));
+	}
+	System.out.println(b);
+	System.out.println(b.ascend());
     }
 }
