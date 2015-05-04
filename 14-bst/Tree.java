895d50515948b9ac0f5cc18a37c709add460d022
Joseph Gelb <jgelb235@verizon.net>
2015-05-04 07:57:04 -0400

    update

--- 14-bst/Tree.java
+++ 14-bst/Tree.java
@@ -1,8 +1,8 @@
 import java.util.*;
-import java.io.*;
 public class Tree{
     private Node root;
     public Tree(){
+	Node head = new Node(MIN_VAlUE);
 	root = null;
     }
     public Tree(Node n){
@@ -28,6 +28,64 @@ public class Tree{
 	    piggy.setRight(new Node(i));
 	}else {
 	    piggy.setLeft(new Node(i));
+	}
+    }
+    /*
+      two pointers to node and parent
+      a)if leaf, set parent left/right to null
+      b)if only has one child, set parent left/right to child
+      c)if 2 children, find largest left or smallest right node and copy data
+      into location of node being removed.
+    */
+    public void remove(int i){
+	Node t = root;
+	Node piggy = t;
+	boolean right = true;
+	while(t.getData()!=i && t!= null ){
+	    piggy = t;
+	    if(t.getData()<i){
+		t=t.getRight();
+		right = true;
+	    } else if(t.getData()>i) {
+		t=t.getLeft();
+		right = false;
+	    }
+	}
+	boolean isRoot = false;
+	if(t == root){
+	    isRoot = true;
+	}
+	if(t.numKids()==0){
+	    if(right){
+		piggy.setRight(null);
+	    }else{
+		piggy.setLeft(null);
+	    }
+	}else if (t.numKids()==1){
+	    if(right){
+		if(t.getRight() != null){
+		    piggy.setRight(t.getRight());
+		}else {
+		    piggy.setRight(t.getLeft());
+		}
+	    } else {
+		if(t.getRight() != null){
+		    piggy.setLeft(t.getRight());
+		}else {
+		    piggy.setLeft(t.getLeft());
+		}
+	    }
+	} else {
+	    Node L = t.getRight();
+	    Node L2 = L;
+	    while (L.getLeft() != null){
+		L2 = L;
+		L = L.getLeft();
+	    }
+	    System.out.println(L2);
+	    t.setData(L.getData());
+	    System.out.println(L2);
+	    L2.setLeft(null);
 	}
     }
     public Node search(int i){
@@ -57,15 +115,11 @@ public class Tree{
     public Node rSearch(int i){
 	return rSearch(root,i);
     }
-    //not really sure how to format a tree but this at least prints everything
     public String traverse(Node t){
 	if(t == null){
 	    return "";
 	}
-	return  traverse(t.getLeft()) +"," + t.getData() +"," + traverse(t.getRight());
-    }
-    public String toString(){
-	return traverse(root);
+	return t.getData() + ", " + traverse(t.getLeft()) + traverse(t.getRight());
     }
     public String ascend(Node t){
 	if(t == null){
@@ -73,55 +127,9 @@ public class Tree{
 	}
 	return ascend(t.getLeft()) + t.getData() + ", " + ascend(t.getRight());
     }
-    public String ascend(){
+    public String toString(){
 	return ascend(root);
     }
-    public void remove(int i){
-	Node t = root;
-	Node piggy = t;
-	boolean right;
-	while(t.getData()!=i && t!=null){
-	    piggy = t;
-	    if(t.getData()<i){
-		t=t.getRight();
-		right = true;
-	    } else if(t.getData()>i) {
-		t=t.getLeft();
-		right = false;
-	    }
-	}
-	if(t.numKids()==0){
-	    if(right){
-		piggy.setRight(null);
-	    }else{
-		piggy.setLeft(null);
-	    }
-	}else if (t.numKids()==1){
-	    if(right){
-		if(t.getRight() != null){
-		    piggy.setRight(t.getRight());
-		}else {
-		    piggy.setRight(t.getLeft());
-		}
-	    } else {
-		if(t.getRight() != null){
-		    piggy.setLeft(t.getRight());
-		}else {
-		    piggy.setLeft(t.getLeft());
-		}
-	    }
-	} else {
-	    L = t.getRight();
-	    L2 = t;
-	    while (L.getLeft() != null){
-		L2 = L
-		    L = L.getLeft();
-	    }
-	    t = L;
-	    L2.setLeft(null);
-	}
-    }
-
     public static void main(String[] args){
 	Random rnd = new Random();
 	Node r = new Node(40);
@@ -130,6 +138,7 @@ public class Tree{
 	    b.insert(10*rnd.nextInt(10));
 	}
 	System.out.println(b);
-	System.out.println(b.ascend());
+	b.remove(40);
+	System.out.println(b);
     }
 }
